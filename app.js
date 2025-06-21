5c533c478a7295b1ad2d91a7b78d333ac776fd20
Tanzeem Ahmad <ahmadtanzeem185@gmail.com>
2025-06-21 17:42:23 +0530

    Create Server.js file, app.js file and setup the server

---
+++ app.js
@@ -0,0 +1,20 @@
+import express, { urlencoded } from 'express';
+import { config } from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { connectDB } from './database/db.js';
+
+export const app = express();
+
+config({ path: "./config/config.env" });
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET" , "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+app.use(cookieParser);
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+connectDB();
